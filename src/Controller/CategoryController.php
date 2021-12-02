04ed6f8770cd1df46dde645412a08ad9dceebc89
CEDRIC_PARDON <cedricp02@gmail.com>
2021-12-02 18:29:00 +0100

    symfony 12

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,9 +6,11 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/category", name="category_")
@@ -24,12 +26,28 @@ class CategoryController extends AbstractController
     public function index(): Response
     {
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
-
-
-        return $this->render(
-            '/category/index.html.twig',
-            ['categories' => $categories]
-        );
+        return $this->render('/category/index.html.twig', ['categories' => $categories]);
+    }
+    /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', ["form" => $form->createView(),]);
     }
     /**
      * Getting a category by name
@@ -40,19 +58,20 @@ class CategoryController extends AbstractController
     public function show(string $categoryName): Response
     {
         $category = $this->getDoctrine()->getRepository(Category::class)->findBy(['name' => $categoryName]);
+        
         if (!$category) {
             throw $this->createNotFoundException(
                 'No category with name : ' . $categoryName
             );
         }
-
         $programs = $this->getDoctrine()->getRepository(Program::class)->findBy(['category' => $category], ['id' => 'DESC'], $limit = 3);
+        
         if (!$programs) {
             throw $this->createNotFoundException(
                 'No program with name : ' . $categoryName
             );
         }
-
+        
         return $this->render('/category/show.html.twig', [
             'categoryName' => $categoryName,
             'programs' => $programs
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,8 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,6 +30,29 @@ class ProgramController extends AbstractController
         $programs = $this->getDoctrine()->getRepository(Program::class)->findAll();
         return $this->render('/program/index.html.twig', ['programs' => $programs]);
     }
+    /**    
+     * The controller for the Program add form    
+     *   
+     * @Route("/new", name="new")   
+     */
+    public function new(Request $request): Response
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->render('program/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
     /**
      * Getting a program by id
      *
@@ -37,6 +62,7 @@ class ProgramController extends AbstractController
     public function show(Program $program): Response
     {
         $seasons = $this->getDoctrine()->getRepository(Season::class)->findBy(['program' => $program], ['number' => 'ASC']);
+
         if (!$seasons) {
             throw $this->createNotFoundException(
                 'No program found . '
