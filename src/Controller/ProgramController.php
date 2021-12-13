aa7d5b0d2bd73ccd3f5385b414892e3a0f9bc457
CEDRIC_PARDON <cedricp02@gmail.com>
2021-12-13 13:53:07 +0100

    symfony 19

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -14,6 +14,8 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Form\ProgramType;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Service\Slugify;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * @Route("/program", name="program_")
@@ -44,9 +46,10 @@ class ProgramController extends AbstractController
      * @Route("/new", name="new")
      */
 
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
+
         $form = $this->createForm(ProgramType::class, $program);
         $form->handleRequest($request);
 
@@ -56,6 +59,12 @@ class ProgramController extends AbstractController
             $program->setSlug($slug);
             $entityManager->persist($program);
             $entityManager->flush();
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('your_email@example.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('program/newProgramEmail.html.twig', ['program' => $program]));
+            $mailer->send($email);
 
             return $this->redirectToRoute('program_index');
         }
